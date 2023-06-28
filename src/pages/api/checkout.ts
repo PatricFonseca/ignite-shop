9de00cf1f8f85e578208db304e29cc5b70e29bbe
Patric de Paiva Fonseca <patric.ccomp@gmail.com>
2023-06-27 21:23:23 -0300

    feat: finish checkout button

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -3,14 +3,23 @@ import { stripe } from '@/lib/stripes'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  checkoutUrl: string | null
+  checkoutUrl?: string | null
+  error?: string
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  const priceId = 'price_1NLbewAGw99nozfPc4wtuZ9F'
+  const { priceId } = req.body
+
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed.' })
+  }
+
+  if (!priceId) {
+    return res.status(400).json({ error: 'Price not found.' })
+  }
 
   const successUrl = `${process.env.NEXT_URL}/success`
   const cancelUrl = `${process.env.NEXT_URL}/`
--- src/pages/product/[productId].tsx
+++ src/pages/product/[productId].tsx
@@ -4,9 +4,11 @@ import {
   ProductContainer,
   ProductDetails,
 } from '@/styles/pages/product'
+import axios from 'axios'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import Stripe from 'stripe'
 
 interface ProductProps {
@@ -21,8 +23,24 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
-  function handleBuyProduct() {
-    console.log(product.defaultPriceId)
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] =
+    useState(false)
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true)
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data
+
+      window.location.href = checkoutUrl
+    } catch (error) {
+      // conectar com uma ferramenta de observabilidade(datadog / sentry)
+      setIsCreatingCheckoutSession(false)
+      alert('Falha ao redirecionar ao checkout!')
+    }
   }
 
   const { isFallback } = useRouter()
@@ -43,7 +61,12 @@ export default function Product({ product }: ProductProps) {
           <span>{product.price}</span>
           <p>{product.description}</p>
 
-          <button onClick={handleBuyProduct}>Comprar agora</button>
+          <button
+            onClick={handleBuyProduct}
+            disabled={isCreatingCheckoutSession}
+          >
+            Comprar agora
+          </button>
         </ProductDetails>
       </ProductContainer>
     </>
